aacae24004f68b91fac16c8ad13fbde585f0cd9c
Hyrhoo <hyrhooo@gmail.com>
2023-05-11 17:12:44 +0200

    rgb slider

--- rgb_slider.py
+++ rgb_slider.py
@@ -6,12 +6,10 @@ class RGB_Slider:
             width: int,
             height: int,
             center: tuple[int, int],
-            angle: int = 0
         ) -> None:
         self.surface = pygame.Surface((1531, 1))
         self.__color_surface()
         self.surface = pygame.transform.scale(self.surface, (width, height))
-        self.surface = pygame.transform.rotate(self.surface, angle)
         self.rect = self.surface.get_rect(center=center)
 
         self.select = False
@@ -89,3 +87,29 @@ class RGB_Slider:
     def get_color(self):
         self.color_change = False
         return self.cursor_color
+
+if __name__ == "__main__":
+    pygame.init()
+    screen_size = (1800, 800)
+    screen = pygame.display.set_mode(screen_size)
+    clock = pygame.time.Clock()
+    rgb_slider = RGB_Slider(1700, 150, tuple(i//2 for i in screen_size))
+
+    while True:
+        screen.fill("#424242")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+            rgb_slider.handle_event(event)
+
+        rgb_slider.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+
+        if rgb_slider.color_change:
+            print(rgb_slider.get_color())
